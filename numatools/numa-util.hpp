92bbd5ec03608bfe59786089b06e41311f212bc5
hy <meJustPlay@163.com>
2016-10-29 13:50:13 +0800

    Developing...

--- numatools/numa-util.hpp
+++ numatools/numa-util.hpp
@@ -32,12 +32,7 @@ namespace numa {
 std::vector<std::string> str_split(const std::string &str, const std::string &del, size_t limit = 0);
 
 bool is_cgroup_avail();
-
 bool is_subsys_avail(const std::string &subsys);
-
-inline bool is_subsys_avail(const char *subsys) {
-    return is_subsys_avail(std::string(subsys));
-}
 
 inline bool is_cpuset_avail() {
     return is_subsys_avail("cpuset");
