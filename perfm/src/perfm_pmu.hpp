0f07a984f5ad9a336e0b76a28e2714f92e1a8646
hy <meJustPlay@163.com>
2016-11-02 20:17:39 +0800

    Developing...

---
+++ perfm/src/perfm_pmu.hpp
@@ -0,0 +1,18 @@
+/**
+ * perfm_pmu.hpp - interface functions for pmu
+ *
+ */
+#ifndef __PERFM_PMU_HPP__
+#define __PERFM_PMU_HPP__
+
+#include <perfmon/pfmlib.h> /* libpfm4 */
+
+namespace perfm {
+
+bool pmu_is_available(pfm_pmu_t pmu);
+
+void pr_pmu_list(bool pr_all = 0);
+
+} /* namespace perfm */
+
+#endif /* __PERFM_PMU_HPP_ */
