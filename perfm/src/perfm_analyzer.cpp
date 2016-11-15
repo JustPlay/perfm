337f1aa6b0972c419b97172d34aab5c54364ab2d
hy <meJustPlay@163.com>
2016-11-15 17:04:49 +0800

    Developing...

--- perfm/src/perfm_analyzer.cpp
+++ perfm/src/perfm_analyzer.cpp
@@ -21,9 +21,9 @@ bool analyzer_t::metric_parse(xml::xml_node<char> *metric)
         return false;
     }
 
-    std::string metric_name(name->value(), name->value_size());
-    std::string metric_formula;
-    std::map<std::string, std::string> formula_alias;
+    std::string m_name(name->value(), name->value_size());
+    std::string m_expr;
+    std::map<std::string, evnam_set_t::iterator> f_alias;
 
     // parsing metric formula (non-empty)
     for (xml::xml_node<char> *node = metric->first_node(); node; node = node->next_sibling()) {
@@ -41,21 +41,22 @@ bool analyzer_t::metric_parse(xml::xml_node<char> *metric)
         if ("event" == nod_nam || "constant" == nod_nam) {
             xml::xml_attribute<char> *attr = node->first_attribute("alias"); 
             if (attr && attr->value_size()) {
-                std::string att_val(attr->value(), attr->value_size());
-                formula_alias.insert({att_val, nod_val});
+                std::string alias_nam(attr->value(), attr->value_size());
+                auto alias_val = this->_evnam.insert(nod_nam).first;
+                f_alias.insert({alias_nam, alias_val});
             }
             continue;
         }
         
         if ("formula" == nod_nam) {
-            metric_formula = nod_val;
+            m_expr = nod_val;
             break;
         }
     }
 
-    if (!metric_name.empty() && !metric_formula.empty() && !formula_alias.empty()) {
-        this->metrics_list.push_back(metric_name);
-        this->formula_list.insert({metric_name, {metric_formula, formula_alias}});
+    if (!m_name.empty() && !m_expr.empty() && !f_alias.empty()) {
+        this->metrics_list.push_back(m_name);
+        this->formula_list.insert({m_name, {m_expr, f_alias}});
     } else {
         perfm_warn("metric's name, formula or formula alias may be empty\n");
         return false;
@@ -96,6 +97,242 @@ bool analyzer_t::metric_parse(const char *filp)
     return true;
 }
 
+std::string analyzer_t::formula_in2postfix(const std::string &formula_infix) const
+{
+    if (formula_infix.empty()) {
+        return "";
+    } 
+
+    enum {
+        ERROR = 0,
+        OPERATOR, // "*/%+-"
+        OPERAND,  
+        BRACKET   // "()"
+    };
+
+    size_t pos = 0;
+
+    auto get_elem = [&pos] (const std::string &str, std::string &res) -> int {
+        const size_t sz_str = str.size(); 
+
+        while (pos < sz_str && std::isspace(str[pos])) { 
+            ++pos;
+        }
+
+        res.clear();
+
+        while (pos < sz_str) {
+            switch (str[pos]) {
+            case ' ':
+                if (res.empty()) {
+                    perfm_fatal("this should never happen\n");
+                }
+
+                return OPERAND;
+
+            case '*':  case '/':  case '%':  case '+':  case '-':
+                if (res.empty()) {
+                    res = str[pos++];
+                    return OPERATOR;
+                } else {
+                    return OPERAND;
+                }
+
+            case '(':  case ')':
+                if (res.empty()) {
+                    res = str[pos++];
+                    return BRACKET;
+                } else {
+                    return OPERAND;
+                }
+
+            default:
+                res += str[pos++];
+            }
+        }
+
+        return res.empty() ? ERROR : OPERAND;
+    };
+
+    int priority[256] = { 0 };
+
+    priority['('] = 0;
+    priority[')'] = 0;
+
+    priority['+'] = 2;
+    priority['-'] = 2;
+
+    priority['*'] = 4;
+    priority['/'] = 4;
+    priority['%'] = 4;
+
+    auto less_priority = [&priority] (int a, int b) -> bool {
+        return priority[a] < priority[b];
+    };
+
+    auto more_priority = [&priority] (int a, int b) -> bool {
+        return priority[a] > priority[b];
+    };
+
+    std::string formula_postfix;
+    std::string elem;
+    std::stack<char> stk;
+
+    auto str_append = [&formula_postfix] (const std::string &str) mutable -> void {
+        formula_postfix += formula_postfix.empty() ? str : " " + str;
+    };
+
+    int stat = get_elem(formula_infix, elem); 
+    while (stat != ERROR) {
+        switch (stat) {
+        case OPERAND:
+            str_append(elem);
+            break;
+
+        case OPERATOR:
+            while (!stk.empty() && !less_priority(stk.top(), elem[0])) {
+                str_append(std::string(1, stk.top()));
+                stk.pop();
+            }
+            stk.push(elem[0]);
+            break;
+
+        case BRACKET:
+            if ("(" == elem) {
+                stk.push('('); 
+            } else {
+                while (!stk.empty() && stk.top() != '(') {
+                    str_append(std::string(1, stk.top()));
+                    stk.pop();
+                }
+                
+                if (!stk.empty()) {
+                    stk.pop();
+                } else {
+                    perfm_fatal("formula invalid %s\n", formula_infix.c_str());
+                }
+            }
+            break;
+        }
+
+        stat = get_elem(formula_infix, elem);
+    }
+
+    while (!stk.empty()) {
+        str_append(std::string(1, stk.top()));
+        stk.pop();
+    }
+
+    return std::move(formula_postfix);
+}
+
+double analyzer_t::formula_eval(const me_formula_t &formula) const
+{
+   /* FIXME */ 
+
+    const auto  f_exprn = formula_in2postfix(formula.first); // std::string
+    const auto &f_alias = formula.second;                    // std::map
+
+    enum {
+        ERROR = 0,
+        OPERATOR,
+        OPERAND
+    };
+
+    size_t pos = 0;
+
+    auto get_elem = [&pos] (const std::string &str, std::string &res) -> int {
+        const size_t sz_str = str.size(); 
+
+        while (pos < sz_str && std::isspace(str[pos])) {
+            ++pos;
+        }
+
+        res.clear();
+
+        while (pos < sz_str) {
+            switch (str[pos]) {
+            case ' ':
+                return OPERAND;
+
+            case '*':  case '/':  case '%':  case '+':  case '-':
+                res = str[pos++];
+                return OPERATOR;
+
+            default:
+                res += str[pos++];
+            }
+        }
+
+        return res.empty() ? ERROR : OPERAND;
+    };
+
+    std::stack<double> stk;
+    std::string elem;
+
+    for (int stat = get_elem(f_exprn, elem); stat != ERROR; stat = get_elem(f_exprn, elem)) {
+        switch (stat) {
+        case OPERAND: {
+                // fetch the real name (event name) for this alias
+                auto nam = f_alias.find(elem);
+                if (nam == f_alias.end()) {
+                    perfm_fatal("invalid operand %s\n", elem.c_str());
+                }
+
+                // fetch data for the given event
+                auto dat = _evdat.find(*nam->second);
+                if (dat == _evdat.end()) {
+                    perfm_fatal("invalid event name %s\n", nam->second->c_str());
+                }
+
+                /* FIXME */
+                stk.push(std::get<0>(dat->second));
+            }
+            break;
+
+        case OPERATOR: {
+                if (stk.size() < 2) {
+                    perfm_fatal("invalid formula expression %s\n", formula.first.c_str()); 
+                }
+
+                double r = stk.top(); stk.pop(); // right operand
+                double l = stk.top(); stk.pop(); // left  operand
+                double v = 0;
+
+                switch (elem[0]) {
+                case '*':
+                    v = l * r;
+                    break;
+
+                case '/':
+                    v = l / r;
+                    break;
+
+                case '+':
+                    v = l + r;
+                    break;
+
+                case '-':
+                    v = l - r;
+                    break;
+
+                default:
+                    perfm_fatal("invalid operator %s\n", elem.c_str()); 
+                }
+
+                stk.push(v);
+            }
+            break;
+        }
+    }
+
+    if (stk.empty()) {
+        perfm_fatal("what ???\n");
+    }
+
+    return stk.top();
+}
+
 void analyzer_t::metric_eval(const std::string &metric)
 {
     auto iter = this->formula_list.find(metric);
@@ -104,19 +341,16 @@ void analyzer_t::metric_eval(const std::string &metric)
         return;
     }
 
-    const auto formula = iter->second.first;  // std::string
-    const auto f_alias = iter->second.second; // std::map
+    double val = formula_eval(iter->second);
 
     /* TODO */
 }
 
-std::string analyzer_t::formula_infix2postfix(const std::string &formula_infix) const
+void analyzer_t::metric_eval()
 {
-    if (formula_infix.empty()) {
-        return "";
-    } 
-
-    /* TODO */
+    for (auto iter = metrics_list.begin(); iter != metrics_list.end(); ++iter) {
+        metric_eval(*iter);
+    }
 }
 
 } /* namespace perfm */
--- perfm/src/perfm_analyzer.hpp
+++ perfm/src/perfm_analyzer.hpp
@@ -6,8 +6,6 @@
 #define __PERFM_ANALYZER_HPP__
 
 #include "perfm_util.hpp"
-#include "perfm_event.hpp"
-#include "perfm_evgrp.hpp"
 #include "perfm_option.hpp"
 
 #include <cstdlib>
@@ -15,6 +13,7 @@
 #include <string>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <tuple>
 
@@ -23,7 +22,13 @@ namespace perfm {
 namespace xml = rapidxml;
 
 class analyzer_t {
-    using pmu_data_t = std::tuple<uint64_t>;
+public:
+    using evnam_set_t = std::unordered_set<std::string>;               /* event name list */
+    using evdat_fmt_t = std::tuple<uint64_t>;                          /* event value fmt */
+    using evdat_map_t = std::unordered_map<std::string , evdat_fmt_t>; /* event data list */
+
+    using metric_nam_t = std::string;
+    using me_formula_t = std::pair<std::string, std::map<std::string, evnam_set_t::iterator>>;
 
 public:
     bool metric_parse(const char *filp);
@@ -34,11 +39,11 @@ public:
 private:
     bool metric_parse(xml::xml_node<char> *metric);
 
-    std::string formula_infix2postfix(const std::string &formula_infix) const;
+    std::string formula_in2postfix(const std::string &formula_infix) const;
+
+    double formula_eval(const me_formula_t &formula) const;
 
 private:
-    using metric_nam_t = std::string;
-    using me_formula_t = std::pair<std::string, std::map<std::string, std::string>>;
     std::unordered_map<metric_nam_t, me_formula_t> formula_list; /* metric = formula
                                                                   * e.g. 
                                                                   *     metric_CPI = a / b
@@ -47,7 +52,8 @@ private:
                                                                   */
     std::vector<metric_nam_t> metrics_list;
 
-    std::unordered_map<std::string, pmu_data_t> metrics_data;
+    evnam_set_t _evnam;   /* event name set */
+    evdat_map_t _evdat;   /* event data set */
 };
 
 } /* namespace perfm */
